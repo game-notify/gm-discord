c1bfa54bcd2d6168bcbc45a7fce5ec42e9d486fd
KasparWe <kaspar.wetsch@gmx.de>
2020-06-26 20:42:20 +0200

    load dotenv

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 
 from bot.bot import DiscordBot
+from dotenv import load_dotenv
+
 
 if __name__ == '__main__':
 
+    load_dotenv()
     bot = DiscordBot()
 
     # set discord bot token from environment var or user input
